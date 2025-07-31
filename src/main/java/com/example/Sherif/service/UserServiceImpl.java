18abf7a7047701585f8a3c89f18c5ada6794ce75
Sherif-toiema22 <sherif.toiema22@gmail.com>
2025-07-31 15:04:56 +0300

    Configuration Security

--- src/main/java/com/example/Sherif/service/UserServiceImpl.java
+++ src/main/java/com/example/Sherif/service/UserServiceImpl.java
@@ -4,12 +4,17 @@ import com.example.Sherif.model.Role;
 import com.example.Sherif.model.User;
 import com.example.Sherif.repository.UserRepository;
 import com.example.Sherif.dto.UserRegistrationDto;
+import org.hibernate.sql.ast.tree.expression.Collation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -33,6 +38,14 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByEmail(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("Invalid username or password");
+
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),mapRolesToAuthorities(user.getRoles()));
+    }
+    private Collection<?extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
